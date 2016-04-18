ed944a0b35a055eb9cd1b4618cd9cdc4b3b4c607
Wnbj <wnbkrumov@gmail.com>
2016-04-18 20:42:26 +0300

    Added Assets Clas

---
+++ src/Assets.java
@@ -0,0 +1,20 @@
+import java.awt.image.BufferedImage;
+
+public class Assets {
+
+    public static BufferedImage background;
+    public static BufferedImage player;
+    public static BufferedImage logo;
+    public static BufferedImage asteroid;
+    public  static BufferedImage blackhole;
+
+    public static void init(){
+        background = ImageLoader.loadImage("/background.jpg");
+        player = ImageLoader.loadImage("/alien.png");
+      //  logo = ImageLoader.loadImage("/GameLogo.png");
+      //  asteroid = ImageLoader.loadImage("/Asteroids/Brown.png");
+      //  blackhole = ImageLoader.loadImage("/blackhole.png");
+
+    }
+
+}
