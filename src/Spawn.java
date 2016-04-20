4965f925c1d44561ae2632622166a68f298e47e5
nikito873 <niki_genchev@abv.bg>
2016-04-21 00:50:45 +0300

    Sound fixes.

--- src/Spawn.java
+++ src/Spawn.java
@@ -33,20 +33,16 @@ public class Spawn {
                 if (!Menu.mute) {
                     AudioPlayer.getMusic("game_background_music").pause();
                     AudioPlayer.getMusic("boss_music").play();
-                }else {
-                    AudioPlayer.getMusic("boss_music").pause();
-                    AudioPlayer.getMusic("game_background_music").play();
                 }
                 handler.addObject(new BossEnemy((Game.WIDTH / 2) - 48, -170, ID.BossEnemy, handler));
             } else if(hud.getLevel() >= 15 && hud.getLevel() % 10 == 5){
                     handler.clearEnemys();
+
                     if (!Menu.mute) {
                         AudioPlayer.getMusic("boss_music").stop();
                         AudioPlayer.getMusic("game_background_music").play();
-                    }else{
-                        AudioPlayer.getMusic("boss_music").pause();
-                        AudioPlayer.getMusic("game_background_music").play();
                     }
+
                     handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
                     handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
                 } else if(hud.getLevel() >= 17 && hud.getLevel() % 10 == 7){
@@ -65,22 +61,20 @@ public class Spawn {
                     handler.addObject(new SmartEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.SmartEnemy, handler));
                 } else if (hud.getLevel() == 10) {
                     handler.clearEnemys();
+
                     if (!Menu.mute) {
                         AudioPlayer.getMusic("game_background_music").stop();
                         AudioPlayer.getMusic("boss_music").play();
-                    }else {
-                        AudioPlayer.getMusic("boss_music").pause();
-                        AudioPlayer.getMusic("game_background_music").play();
                     }
+
                     handler.addObject(new BossEnemy((Game.WIDTH / 2) - 48, -170, ID.BossEnemy, handler));
                 } else if (hud.getLevel() == 15) {
+
                     if (!Menu.mute) {
                         AudioPlayer.getMusic("boss_music").stop();
                         AudioPlayer.getMusic("game_background_music").play();
-                    }else{
-                        AudioPlayer.getMusic("boss_music").pause();
-                        AudioPlayer.getMusic("game_background_music").play();
                     }
+
                     handler.clearEnemys();
                     handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
                     handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemy, handler));
@@ -91,23 +85,21 @@ public class Spawn {
                     handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
                 } else if (hud.getLevel() == 20) {
                     handler.clearEnemys();
+
                     if (!Menu.mute) {
                         AudioPlayer.getMusic("game_background_music").stop();
                         AudioPlayer.getMusic("boss_music").play();
-                    }else{
-                        AudioPlayer.getMusic("boss_music").pause();
-                        AudioPlayer.getMusic("game_background_music").play();
                     }
+
                     handler.addObject(new BossEnemy((Game.WIDTH / 2) - 300, -170, ID.BossEnemy, handler));
                     handler.addObject(new BossEnemy((Game.WIDTH / 2) + 100, -170, ID.BossEnemy, handler));
                 } else if (hud.getLevel() == 24) {
+
                     if (!Menu.mute) {
                         AudioPlayer.getMusic("boss_music").stop();
                         AudioPlayer.getMusic("game_background_music").play();
-                    }else{
-                        AudioPlayer.getMusic("boss_music").pause();
-                        AudioPlayer.getMusic("game_background_music").play();
                     }
+
                     handler.clearEnemys();
                     handler.addObject(new SmartEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.SmartEnemy, handler));
                 } else if (hud.getLevel() == 25) {
